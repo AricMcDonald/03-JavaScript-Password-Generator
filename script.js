ae1e05a6946ed178843d71afe41ca130b7fd224a
AricMcDonald <aricmcdonald@gmail.com>
2021-12-09 20:42:55 -0700

    readme updated

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var generateBtn = document.querySelector("#generate");
 var charsUp = ['A', 'B', 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L', 'N', 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z'];
-//"ABCDEFGHIJKLMNOPQRSTUVWXTZ";
+
 var charsLow = ['a', 'b', 'c' , 'd' , 'e' , 'f,' , 'g' , 'h' , 'i' , 'j' , 'k', 'l' , 'm' , 'o' , 'p' , 'q' , 'r' , 's' , "t" , 'u' , 'v', 'w' , 'x' , 'y' , 'z']
-//"abcdefghiklmnopqrstuvwxyz";
+
 var num = ['1', '2', '3' , '4' , '5' , '6', '7' , '8' , '9' , '0'] 
-//"0123456789"
+
 var symb = ['!', '@', '#', '$' , '%' , '^' , '&' , '*' , '(' , ')' , '-' , '+' , '_' , '=' , '[' , ']' , '{' , '}' , '|' , ':' , '"' , ';' , ',' , '<' , '.' , ">", '?'];
-//"!@#$%^&*()_+,./;'[\]=-|~`";
+
 var charset = []
 var genPass = []
 
